f2cab9fff75d8e4a52f03b62db81dc6dc6a87079
chulopapi <marlon.guandique@gmail.com>
2020-06-10 21:26:14 -0700

    cleaning up the js file and adding the README.md file

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # passwordgenerator
 Javascript Password Generator
+
+
+The program generator program was designed to randomly generate a password that meets certain a strong password that provides greater security for the user.
+
+
+The programs follows the following criteria:
+
+1. The user is prompt with a landing page that will be prompt to hit a password generator button.
+
+<img width="1463" alt="Screen Shot 2020-06-10 at 8 55 05 PM" src="https://user-images.githubusercontent.com/14985358/84344003-7c5d8280-ab5e-11ea-973a-c5def4f37e29.png">
+
+2.  Once the user is asked to provide a password lenght that is desired, it will check that the number is between the following range: 8 to 128 characters.  The program checks for only numbers, if there are letters, negative numbers, or any number lower than 8 or greater than 128, the program runs a loop until the correct number is provide.
+
+<img width="913" alt="Screen Shot 2020-06-10 at 9 12 01 PM" src="https://user-images.githubusercontent.com/14985358/84344262-2b01c300-ab5f-11ea-9ba5-9057c2ede648.png">
+
+
+3. The program ask for the user to select the following prompts for characters selection:
+    a. special characters
+    b. numbers
+    c. lower letters
+    d. upper letters.  
+
+The following is an example of a prompt:
+
+<img width="339" alt="Screen Shot 2020-06-10 at 9 20 05 PM" src="https://user-images.githubusercontent.com/14985358/84344655-34d7f600-ab60-11ea-9467-52770f6cd6cc.png">
+
+4.  If the user does not provide at least one prompt, the program will exit and will display the following:
+
+<img width="882" alt="Screen Shot 2020-06-10 at 9 17 38 PM" src="https://user-images.githubusercontent.com/14985358/84344889-c7789500-ab60-11ea-8f95-f2f9d4b1fe41.png">
+
+
+5.  Once the user has selected at least one option, the password is displayed:
+
+<img width="865" alt="Screen Shot 2020-06-10 at 9 14 03 PM" src="https://user-images.githubusercontent.com/14985358/84344443-ab282880-ab5f-11ea-9bf3-4a7659fcf1d6.png">
+
+
+
+
+
--- script.js
+++ script.js
@@ -4,10 +4,10 @@
 
 let pw = [];
 let setOfRemaining = [];
-let spCh = ["#", "%","^","&","*","%","!","@","(",")","+","-"]
+let spCh = ["#", "%", "^", "&", "*", "%", "!", "@", "(", ")", "+", "-"]
 let numbs = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-let upperletters = ["A", "B", "C", "D", "E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","O","W","X","Y","Z"]
-let lowerletters = ["a", "b", "c", "d", "e", "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","o","w","x","y","z",]
+let upperletters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "O", "W", "X", "Y", "Z"]
+let lowerletters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "o", "w", "x", "y", "z",]
 
 // function to start password generation
 function generatePassword() {
@@ -16,22 +16,17 @@ function generatePassword() {
 
   var passwordLength = 0
   while ((isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128)) {
-   passwordLength = parseInt(window.prompt("Please provide password length (min 8 and max 128)"));
+    passwordLength = parseInt(window.prompt("Please provide password length (min 8 and max 128)"));
   }
 
   // console to debugging
   console.log("password length: ", passwordLength);
   console.log("type of password length: ", typeof (passwordLength));
- 
-  //var specialCharacter = "false"
-  //while (specialCharacter = window.confirm("Do you need special character")) {
-   //(specialCharacter) 
-   // let index = Math.floor(Math.random() * 100) % spCh.length
-  // }
-  
-   var specialCharacter = window.confirm("Do you need special character");
-   if (specialCharacter) {
-      let index = Math.floor(Math.random() * 100) % spCh.length
+
+
+  var specialCharacter = window.confirm("Do you need special character");
+  if (specialCharacter) {
+    let index = Math.floor(Math.random() * 100) % spCh.length
 
     pw.push(spCh[index])
     setOfRemaining = setOfRemaining.concat(spCh)
@@ -40,52 +35,54 @@ function generatePassword() {
   var wantNumbers = window.confirm("Do you want to include numbers?");
   if (wantNumbers) {
     let index = Math.floor(Math.random() * 394) % numbs.length;
-    
+
     pw.push(numbs[index])
     setOfRemaining = setOfRemaining.concat(numbs)
   }
   var uletters = window.confirm("What about Upper Letters?");
   if (uletters) {
-      let index = Math.floor(Math.random() * 394) % upperletters.length;
+    let index = Math.floor(Math.random() * 394) % upperletters.length;
 
-      pw.push(upperletters[index])
-      setOfRemaining = setOfRemaining.concat(upperletters)
-  }    
+    pw.push(upperletters[index])
+    setOfRemaining = setOfRemaining.concat(upperletters)
+  }
 
   var lletters = window.confirm("Do you want Lower Letters?");
   if (lletters) {
-      let index = Math.floor(Math.random() * 394) % lowerletters.length;
-      pw.push(lowerletters[index])
-  }  
-  
-  if(!lletters && !uletters && !wantNumbers && !specialCharacter) {
-    alert("Ummmm how do you expect me to make a password")
+    let index = Math.floor(Math.random() * 394) % lowerletters.length;
+    pw.push(lowerletters[index])
+  }
+
+  //The user needs to at least choose one character category
+
+  if (!lletters && !uletters && !wantNumbers && !specialCharacter) {
+    alert("Ummmm.. how do you expect me to make a password?")
     return;
   }
 
   // adding additional characters to setOfRemaining array
-  setOfRemaining.sort((a,b) => Math.random() - .50)
+  setOfRemaining.sort((a, b) => Math.random() - .50)
   let diff = passwordLength - pw.length;
-  if(diff > 0) {
-    for(let i = 0; i < diff; i++) {
+  if (diff > 0) {
+    for (let i = 0; i < diff; i++) {
       pw.push(setOfRemaining[i])
     }
   }
   //displaying in console
   console.log("pw array", pw)
   console.log("pw joined", pw.join(""))
-// display the password created
+  // display the password created
   return pw.join("");
- 
+
 }
-    // Assignment Code
-    var generateBtn = document.querySelector("#generate");
-    // Add event listener to generate button
-    generateBtn.addEventListener("click", function (e) {
-      console.log("Generate Password clicked", e);
-      writePassword();
-   
-    });
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", function (e) {
+  console.log("Generate Password clicked", e);
+  writePassword();
+
+});
 
 // Write password to the #password input
 function writePassword() {
